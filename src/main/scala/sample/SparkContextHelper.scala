a0218757515f766fe0d354b8d773843ca26e27b5
notyy <notyycn@gmail.com>
2014-09-16 15:16:13 +0800

    extract a SparkContextHelper

---
+++ src/main/scala/sample/SparkContextHelper.scala
@@ -0,0 +1,23 @@
+package sample
+
+import com.typesafe.scalalogging.slf4j.LazyLogging
+import org.apache.spark.{SparkConf, SparkContext}
+
+trait SparkContextHelper extends LazyLogging {
+
+  def withSparkContext[T](sparkConf: SparkConf)(f: SparkContext => T): T = {
+    logger.info("starting spark context")
+    val sc = new SparkContext(sparkConf)
+    logger.info("spark context started")
+    try {
+      logger.info("calling function on spark context")
+      val rs = f(sc)
+      logger.info("function completed")
+      rs
+    }finally {
+      logger.info("stopping spark context")
+      sc.stop()
+      logger.info("spark context stopped")
+    }
+  }
+}
